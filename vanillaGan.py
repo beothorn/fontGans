2cec4ee319d5f8d27c08a4b5b64b01a579bf1fad
lucass <lucass@gmail.com>
2020-09-22 20:19:11 +0200

    More samples and shuffle

--- vanillaGan.py
+++ vanillaGan.py
@@ -7,38 +7,64 @@ from numpy import random
 from matplotlib import pyplot as plt
 
 
-def view_samples(samples, m, n):
-    fig, axes = plt.subplots(figsize=(10, 10), nrows=m, ncols=n, sharey=True, sharex=True)
-    for ax, img in zip(axes.flatten(), samples):
-        ax.xaxis.set_visible(False)
-        ax.yaxis.set_visible(False)
-        im = ax.imshow(1-img.reshape((2,2)), cmap='Greys_r')
-    plt.show()
+def good_face_gen():
+    return [random.uniform(0.8, 1), random.uniform(0, 0.2), random.uniform(0, 0.2), random.uniform(0.8, 1)]
+
+
+def bad_face_gen():
+    return np.array([random.uniform(0, 0.7), random.uniform(0.3, 1), random.uniform(0.3, 1), random.uniform(0, 0.7)])
+
+
 
 
 # Examples of faces
-good_faces_x = np.array([np.array([1, 0, 0, 1]),
-                         np.array([0.9, 0.1, 0.2, 0.8]),
-                         np.array([0.9, 0.2, 0.1, 0.8]),
-                         np.array([0.8, 0.1, 0.2, 0.9]),
-                         np.array([0.8, 0.2, 0.1, 0.9])])
-good_faces_y = np.column_stack((np.ones(5), np.zeros(5)))
+# good_faces_x = np.array([np.array([1, 0, 0, 1]),
+#                         np.array([0.9, 0.1, 0.2, 0.8]),
+#                         np.array([0.9, 0.2, 0.1, 0.8]),
+#                         np.array([0.8, 0.1, 0.2, 0.9]),
+#                         np.array([0.8, 0.2, 0.1, 0.9])])
 
-bad_faces_x = np.array([np.random.randn(4) for i in range(20)])
-bad_faces_y = np.column_stack((np.zeros(20), np.ones(20)))
 
-faces_x = np.concatenate((good_faces_x, bad_faces_x))
-faces_y = np.concatenate((good_faces_y, bad_faces_y))
+faces_x = np.array([good_face_gen()])
+faces_y = np.array([1])
+
+
+
+for i in range(2000):
+    if random.uniform(0, 1) > 0.5:
+        faces_x = np.append(faces_x, [good_face_gen()], axis = 0)
+        faces_y = np.append(faces_y, [1], axis=0)
+    else:
+        faces_x = np.append(faces_x, [bad_face_gen()], axis = 0)
+        faces_y = np.append(faces_y, [0], axis=0)
+
+
+test_faces_x = np.array([good_face_gen()])
+test_faces_y = np.array([1])
+
+for i in range(200):
+    if random.uniform(0, 1) > 0.5:
+        test_faces_x = np.append(test_faces_x, [good_face_gen()], axis = 0)
+        test_faces_y = np.append(test_faces_y, [1], axis=0)
+    else:
+        test_faces_x = np.append(test_faces_x, [bad_face_gen()], axis = 0)
+        test_faces_y = np.append(test_faces_y, [0], axis=0)
 
 model = tf.keras.models.Sequential([
+  Dense(4, activation='relu'),
+  Dense(4, activation='relu'),
+  Dense(4, activation='relu'),
   Dense(4, activation='relu'),
   Dense(2, activation='softmax')
 ])
 
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 model.fit(faces_x, faces_y, epochs=30)
 
-print(model(faces_x))
+result = model.predict(test_faces_x)
+
+for i, r in enumerate(result):
+    print(f"Was {r[0] < r[1]} and should be {test_faces_y[i] == 1} : {test_faces_x[i]} ")
