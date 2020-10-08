6d9fb6c6e2516f8cd33a295d57697f8c4d294807
lucass <lucass@gmail.com>
2020-10-09 00:53:51 +0200

    Regras do gerador de estrela

---
+++ stars/starGenerator.py
@@ -0,0 +1,12 @@
+
+# tamanho minimo
+# Ponto aleatrio nessa area de 50x50
+# mas a partir da borda esquerda + minimo ate a borda direita - minimo
+# topo + minimo e fundo - minimo
+# Mede a distancia pra borda mais proxima
+# tamanho aleatorio entre o minimo e a distancia medida
+# angulo aleatorio entre 0 360
+# acha o ponto do circulo nesse angulo
+# repete 4 vezes: soma 144 + variaçao pequena entre -3 e +3 (vai dar 5 segmentos formando uma estrela)
+# da pra usar PILLOW pra desenhar os testes
+
